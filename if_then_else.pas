d90f7778f6afcafea46fa67689868bfb3020c183
ZakyZaujan <48984157+fleshbones7@users.noreply.github.com>
2019-07-11 14:04:41 +0700

    Update if_then_else.pas

--- if_then_else.pas
+++ if_then_else.pas
@@ -1,1 +1,17 @@
+Program Lulus;
+Uses crt;
+Var nilai : Real;
+Begin
+Write ('Jumlah Nilai :');
+Readln (nilai);
 
+If nilai > 60 Then Writeln('Lulus');
+
+Readln;
+Else
+      Begin
+      Writeln('Tidak Lulus');
+      End;
+
+   Readln;
+End.
